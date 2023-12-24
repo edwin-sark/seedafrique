fe2a921752dfa095f976128fff777d8545e6ca1e
edwin-sark <116862715+edwin-sark@users.noreply.github.com>
2023-12-24 11:30:34 -0800

    Creating NavBar component

---
+++ next-app/app/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Services</li>
+            <li>Gallery</li>
+            <li>Blog</li>
+            <li>Store</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
